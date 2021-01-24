9d9fb8f08e050a01b0177a2067d5dcdd5e412a81
Harald Gliebe <harald.gliebe@gmail.com>
2021-01-24 23:54:30 +0100

    cleanup

--- src/day24.nix
+++ src/day24.nix
@@ -27,35 +27,30 @@ let
     in foldl' go {g = grid; a=acc; ps=[];} nbs;
 
   step = acc@{g, a, ps}: n:
-    if ps == [] then acc
+    if ps == [] then acc.a
     else let
       nbs = unique (concatLists (map neighbors ps));
-    in deepSeq (nbs) (update g nbs n a);
+    in deepSeq (nbs) (step (update g nbs n a) (n+1));
 
-  findDistances = grid: count: start: j:
+  findDistances = grid: maxNum: start: j:
     let
-      a = genList (x: 0) count;
+      a = genList (x: 0) maxNum;
       g = updatePos grid start.x start.y "#";
       acc = {inherit g a; ps=[ start ];};
     in
-      (foldl' step acc (range 1 10000)).a;
+      step acc 1;
 
   solve = file: maxNum:
     let
-      count = maxNum + 1;
       grid = map stringToCharacters (lines (readFile file));
       posLineOf = l: j: findFirst (x: x > -1) (-1) (zipListsWith (v: ix: if v == j then ix else -1) l (range 0 (length l)));
       posOf = grid: j: findFirst (p: p != -1) (-1) (zipListsWith (v: ix: let x = posLineOf v j; in if x == -1 then -1 else {y = ix; x=x;}) grid (range 0 (length grid)));
       positions = map (posOf grid) (map toString (range 0 maxNum));
-      distances = map (j: findDistances grid count (elemAt positions j)  j) (range 0 maxNum);
+      distances = map (j: findDistances grid (maxNum + 1) (elemAt positions j)  j) (range 0 maxNum);
       perms = permutations (range 1 maxNum);
       result = minimum 99999 (map (cost distances) perms);
       result2 = minimum 99999 (map (cost2 distances) perms);
-    in
-      {
-        inherit positions distances result result2;
-        # test = findDistances grid count (elemAt positions maxNum)  count;
-      };
+    in { inherit result result2; };
 
   cost = distances: perm:
     let go = ds: acc: ps:
